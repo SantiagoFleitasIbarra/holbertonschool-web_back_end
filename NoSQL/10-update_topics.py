4ccb40b3d438c918bd953e6e6b6aef8336079d63
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-20 15:41:41 +0000

    add task

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,7 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """"""
+    """Function that changes all topics of a school"""
     name_school = {"name": name}
     list_topics = {"$set": {"topics": topics}}
     mongo_collection.update_many(name_school, list_topics)
