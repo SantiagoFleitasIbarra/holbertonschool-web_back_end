20b8cba97cb20c5f898a0d661e0f731e18a3da66
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-30 15:34:54 +0000

    fixed bug

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-        let str1 = string.split("")
-        return array1.concat(array2, str1)
+    return array1.concat(array2, string.split(''))
 }
