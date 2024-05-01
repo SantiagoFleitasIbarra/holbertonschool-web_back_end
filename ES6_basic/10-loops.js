8f92634921b7921c9748e58c70cd73dc47b026b1
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-01 22:57:10 +0000

    fixed bug

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+    const list = []
     for (let value of array) {
-            value = appendString + value;
+            list.push(appendString + value);
     }
-    return array;
+    return list;
 }
