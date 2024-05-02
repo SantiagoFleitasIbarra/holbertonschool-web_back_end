73469c4e00c27453728fc7d1a83f6234ad791d4e
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-02 00:15:45 +0000

    fixed style error

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const list = []
-    for (let value of array) {
-            list.push(appendString + value);
-    }
-    return list;
+  const list = [];
+  for (let value of array) {
+    list.push(appendString + value);
+  }
+  return list;
 }
