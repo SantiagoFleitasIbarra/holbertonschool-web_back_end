c3748ebc0f1b791e7dc2c200e509d57d78220486
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-08 19:11:05 -0300

    add task

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+const brand = Symbol();
+const motor = Symbol();
+const color = Symbol();
+
+export default class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+
+    cloneCar() {
+      return new this.constructor(this._brand, this._motor, this._color);
+    }
+}
