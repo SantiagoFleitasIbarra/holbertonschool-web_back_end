3875bda40874f0c87faa1c989c7e11e086435fc4
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-12 20:51:31 +0000

    add task

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(function (response, error) {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+    if (error) {
+      console.error(error);
+    }
+  });
+}
