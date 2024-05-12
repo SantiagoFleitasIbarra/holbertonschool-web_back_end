0e8716cade817e5a2dcc99f4a2d7ae5b96619bd5
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-12 21:18:18 +0000

    add task and fix bug style

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const photo = values[0];
+      const user = values[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
